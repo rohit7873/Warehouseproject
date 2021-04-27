8d7a3967d16227daa9b628c144d27bee0e0ea673
rohitj2ee <rohitsonawane5577@gmail.com>
2021-04-27 08:47:32 +0530

    create the warehouseservice and added the getAllWarehouse methods

---
+++ WarehouseApplication/src/main/java/com/nt/demo/service/Warehouseservice.java
@@ -0,0 +1,12 @@
+package com.nt.demo.service;
+
+import java.util.List;
+
+public class Warehouseservice {
+
+	public List<String> getAllWareHouseName()
+	{
+		System.out.println("fetching the warehouses");
+		return null;
+	}
+}
