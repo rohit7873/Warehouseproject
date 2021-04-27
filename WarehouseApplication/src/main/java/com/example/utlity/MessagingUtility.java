9977973d000fbaf7c4d2a14526beb0057b175b35
rohitj2ee <rohitsonawane5577@gmail.com>
2021-04-27 23:00:45 +0530

    adding the messaging feture

---
+++ WarehouseApplication/src/main/java/com/example/utlity/MessagingUtility.java
@@ -0,0 +1,11 @@
+package com.example.utlity;
+
+public class MessagingUtility {
+
+	public  void sendMessage()
+	{
+		/**todo:
+		 * here we write the code to send the message to user
+		 * */
+	}
+}
